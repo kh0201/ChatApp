f38abd187e00bbd015ee76b074f4965f99894e40
cheesegarlic <theloneliest21@gmail.com>
2018-09-26 13:30:01 +0900

    서버에서 정보 받아오기 전 클라 보여주기 위한 세팅

--- Chat-Angular/src/app/accountinfo/accountinfo.component.ts
+++ Chat-Angular/src/app/accountinfo/accountinfo.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { LoginService } from '../login.service';
+import { loginInfo } from '../loginInfo';
 
 @Component({
   selector: 'app-accountinfo',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountinfoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService: LoginService,
+    private location: Location) { }
 
   ngOnInit() {
+    this.getInfo();
   }
 
+  getInfo(): void {
+    //this.loginService.GetInfo();
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+
+  @Input() accountDetail: loginInfo;
 }
--- Chat-Angular/src/app/app.module.ts
+++ Chat-Angular/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ChatService} from './chat.service';
-import { LoginService} from './login.service';
+import { FormsModule } from '@angular/forms';
+import { ChatService } from './chat.service';
+import { LoginService } from './login.service';
 import { WebSocketService } from './web-socket.service';
 import { AuthGuard } from './auth.guard';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { ApproutingModule} from './approuting/approuting.module';
+import { ApproutingModule } from './approuting/approuting.module';
 import { ChatComponent } from './chat/chat.component';
 import { AccountinfoComponent } from './accountinfo/accountinfo.component';
 
@@ -21,13 +22,14 @@ import { AccountinfoComponent } from './accountinfo/accountinfo.component';
   ],
   imports: [
     BrowserModule,
-	ApproutingModule
+    FormsModule,
+    ApproutingModule
   ],
   providers: [
-	ChatService,
-	LoginService,
-  WebSocketService,
-  AuthGuard
+    ChatService,
+    LoginService,
+    WebSocketService,
+    AuthGuard
   ],
   bootstrap: [AppComponent]
 })
